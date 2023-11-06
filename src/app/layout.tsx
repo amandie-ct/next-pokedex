a9bb18710883864f8cd34f1007d647112dbaf1a6
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2023-11-05 22:26:04 -0300

    adding providers and global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import StyledJsxRegistry from '@/lib/registry'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledJsxRegistry>
+          <Providers>{children}</Providers>
+        </StyledJsxRegistry>
+      </body>
     </html>
   )
 }
---
+++ src/app/providers.tsx
@@ -0,0 +1,11 @@
+import GlobalStyles from '@/styles/global'
+import React, { PropsWithChildren } from 'react'
+
+export function Providers({ children }: PropsWithChildren) {
+  return (
+    <>
+      <GlobalStyles />
+      {children}
+    </>
+  )
+}
