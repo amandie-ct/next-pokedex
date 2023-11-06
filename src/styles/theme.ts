ed1594044f9a660390a39d9e0110139d4ae29fa0
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2023-11-05 23:43:07 -0300

    adding theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,48 @@
+export default {
+    grid: {
+      container: '130rem',
+      gutter: '3.2rem'
+    },
+    border: {
+      radius: '0.4rem'
+    },
+    font: {
+      family:
+        "Quicksand, sans-serif",
+      light: 300,
+      normal: 400,
+      bold: 600,
+      sizes: {
+        xsmall: '1.2rem',
+        small: '1.4rem',
+        medium: '1.6rem',
+        large: '1.8rem',
+        xlarge: '2.0rem',
+        xxlarge: '2.8rem'
+      }
+    },
+    colors: {
+      white: '#fcfcfc',
+      gray1: '#c0c0c0',
+      gray2: '#D3D3D3',
+      gray3: '#adadad',
+      gray4: '#2b2d2f',
+      black: '#030517',
+    },
+    spacings: {
+      xxsmall: '0.8rem',
+      xsmall: '1.6rem',
+      small: '2.4rem',
+      medium: '3.2rem',
+      large: '4.0rem',
+      xlarge: '4.8rem',
+      xxlarge: '5.6rem'
+    },
+    layers: {
+      base: 10,
+      menu: 20,
+      overlay: 30,
+      modal: 40,
+      alwaysOnTop: 50
+    }
+}  
