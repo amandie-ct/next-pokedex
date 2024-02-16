4efdec13f595cbee4d4bfd6e014cd3fee8b627b9
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-02-15 22:24:56 -0300

    removing unused imports

--- src/app/pokemon/[pokemonId]/page.tsx
+++ src/app/pokemon/[pokemonId]/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 import Theme from '@/styles/theme'
 import { ThemeProvider } from 'styled-components'
-import { Provider, useSelector } from 'react-redux'
-import store, { RootState } from '../../../lib/store'
+import { Provider } from 'react-redux'
+import store from '../../../lib/store'
 import PokemonDetails from '../../components/PokemonDetails'
 
 export default function Pokemon({ params }: { params: { pokemonId: number } }) {
